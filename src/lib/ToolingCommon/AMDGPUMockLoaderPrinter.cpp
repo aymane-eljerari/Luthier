530621b3b7f59e6c812252ff5124203651295ed0
matinraayai <30674652+matinraayai@users.noreply.github.com>
2026-01-10 22:26:24 -0500

    Cleaned up the formatting of the disassembled instructions + added print for non-execute contents.

--- src/lib/ToolingCommon/AMDGPUMockLoaderPrinter.cpp
+++ src/lib/ToolingCommon/AMDGPUMockLoaderPrinter.cpp
@@ -34,6 +34,12 @@
 
 namespace luthier {
 
+static void printBytes(llvm::ArrayRef<uint8_t> Bytes, llvm::raw_ostream &OS) {
+  llvm::interleave(
+      Bytes, [&](uint8_t Byte) { OS << llvm::format("%02X", Byte); },
+      [&]() { OS << " "; });
+}
+
 static void printELFType(const llvm::object::ELF64LE::Phdr &Phdr,
                          llvm::raw_ostream &OS) {
   switch (Phdr.p_type) {
@@ -95,7 +101,7 @@ static void printELFType(const llvm::object::ELF64LE::Phdr &Phdr,
 
 AMDGPUMockLoaderPrinter::AMDGPUMockLoaderPrinter(llvm::raw_ostream &OS)
     : OS(OS) {
-  /// Need to initialize the
+  /// Need to initialize the disassembler
   LLVMInitializeAMDGPUDisassembler();
 }
 
@@ -181,62 +187,64 @@ AMDGPUMockLoaderPrinter::run(llvm::Module &M,
                                    "{0} for Triple {1}.",
                                    Target, TT.normalize())));
 
-    auto MCCtx =
-        std::make_unique<llvm::MCContext>(TT, MAI.get(), MRI.get(), STI.get());
+    llvm::MCContext MCCtx(TT, MAI.get(), MRI.get(), STI.get());
 
     auto DisAsm = std::unique_ptr<llvm::MCDisassembler>(
-        Target->createMCDisassembler(*STI, *MCCtx));
+        Target->createMCDisassembler(*STI, MCCtx));
 
     LUTHIER_CTX_EMIT_ON_ERROR(
         Ctx, LUTHIER_GENERIC_ERROR_CHECK(DisAsm.get(),
                                          "Failed to create a disassembler"));
 
-    OS << "Code Object Index: " << CodeObjectIdx << "\n";
-    OS << llvm::formatv(
-        "\tLoad Base Address: {0:x}\n",
-        reinterpret_cast<uint64_t>(LCO.getLoadedRegion().data()));
-    OS << llvm::formatv("\tLoad size: {0:x}\n", LCO.getLoadedRegion().size());
+    uint64_t LoadBase =
+        reinterpret_cast<uint64_t>(LCO.getLoadedRegion().data());
 
-    OS << "Loaded Segments: \n";
-
+    OS << "Loaded Code Object #" << CodeObjectIdx << "\n";
+    OS << llvm::formatv("- Load Base Address: {0:x}\n", LoadBase);
+    OS << llvm::formatv("- Load size: {0:x}\n", LCO.getLoadedRegion().size());
+    OS << "- Segment Program Headers: \n";
     for (const auto &[PHIdx, PH] : llvm::enumerate(LCO.getLoadSegments())) {
-      OS << "PH Idx: " << PHIdx << "\n";
-      constexpr auto Fmt = "0x%016" PRIx64 " ";
-      OS << "\tType: ";
+      OS.indent(2) << "- Idx: " << PHIdx << "\n";
+      OS.indent(2) << "- Type: ";
       printELFType(*PH, OS);
       OS << "\n";
-      OS << "\tOffset: "
-         << llvm::format(Fmt, static_cast<uint64_t>(PH->p_offset)) << "\n";
-      OS << "\tVAddr: " << llvm::format(Fmt, static_cast<uint64_t>(PH->p_vaddr))
-         << "\n";
-      OS << "\tPAddr: " << llvm::format(Fmt, static_cast<uint64_t>(PH->p_paddr))
-         << "\n";
-      OS << llvm::format("\talign 2**%u",
-                         llvm::countr_zero<uint64_t>(PH->p_align))
-         << "\n";
-      OS << "\tFilesz: "
-         << llvm::format(Fmt, static_cast<uint64_t>(PH->p_filesz)) << "\n";
-      OS << "\tMemsz: " << llvm::format(Fmt, static_cast<uint64_t>(PH->p_memsz))
-         << "\n";
-      OS << "\tFlags: " << ((PH->p_flags & llvm::ELF::PF_R) ? "r" : "-")
-         << ((PH->p_flags & llvm::ELF::PF_W) ? "w" : "-")
-         << ((PH->p_flags & llvm::ELF::PF_X) ? "x" : "-") << "\n";
+      OS.indent(2) << llvm::formatv("- Offset: {0:x}\n",
+                                    static_cast<uint64_t>(PH->p_offset));
+      OS.indent(2) << llvm::formatv("- VAddr: {0:x}\n",
+                                    static_cast<uint64_t>(PH->p_vaddr));
+      OS.indent(2) << llvm::formatv("- PAddr: {0:x}\n",
+                                    static_cast<uint64_t>(PH->p_paddr));
+      OS.indent(2) << llvm::format("- Alignment: 2**%u",
+                                   llvm::countr_zero<uint64_t>(PH->p_align))
+                   << "\n";
+      OS.indent(2) << llvm::formatv("- Filesz: {0:x}\n",
+                                    static_cast<uint64_t>(PH->p_filesz));
+      OS.indent(2) << llvm::formatv("- Memsz: {0:x}\n",
+                                    static_cast<uint64_t>(PH->p_memsz));
+      OS.indent(2) << "- Flags: "
+                   << ((PH->p_flags & llvm::ELF::PF_R) ? "r" : "-")
+                   << ((PH->p_flags & llvm::ELF::PF_W) ? "w" : "-")
+                   << ((PH->p_flags & llvm::ELF::PF_X) ? "x" : "-") << "\n";
+      OS.indent(2) << "----------\n";
+    }
 
+    OS << "- Loaded Contents:\n";
+    for (const auto &[PHIdx, PH] : llvm::enumerate(LCO.getLoadSegments())) {
+      OS << "Segment #" << PHIdx << ":\n";
+      uint64_t SegmentCurrAddr =
+          reinterpret_cast<uint64_t>(LCO.getLoadedRegion().data()) +
+          PH->p_vaddr;
+      uint64_t SegmentEndAddr = SegmentCurrAddr + PH->p_filesz;
       if ((PH->p_flags & llvm::ELF::PF_X)) {
-        OS << "\tSegment disassembly:\n";
-        /// Disassemble both the loaded segmet
-        uint64_t SegmentCurrAddr =
-            reinterpret_cast<uint64_t>(LCO.getLoadedRegion().data()) +
-            PH->p_vaddr;
-        uint64_t SegmentEndAddr = SegmentCurrAddr + PH->p_filesz;
         uint64_t MaxReadSize = MAI->getMaxInstLength();
         while (SegmentCurrAddr < SegmentEndAddr) {
-          OS << llvm::formatv("Addr: {0:x}, Inst: ", SegmentCurrAddr);
+          OS.indent(2) << llvm::formatv("// {0:x}: ",
+                                        SegmentCurrAddr - LoadBase);
           size_t ReadSize = (SegmentCurrAddr + MaxReadSize) < SegmentEndAddr
                                 ? MaxReadSize
                                 : SegmentEndAddr - SegmentCurrAddr;
           llvm::MCInst Inst;
-          size_t InstSize{};
+          size_t InstSize{0};
           llvm::ArrayRef ReadBytes = {
               reinterpret_cast<uint8_t *>(SegmentCurrAddr), ReadSize};
 
@@ -248,15 +256,26 @@ AMDGPUMockLoaderPrinter::run(llvm::Module &M,
                        llvm::formatv(
                            "Failed to disassemble instruction at address {0:x}",
                            SegmentCurrAddr)));
+          printBytes(llvm::ArrayRef(ReadBytes.data(), InstSize), OS);
+          OS << "\n  |->";
           IP->printInst(&Inst, SegmentCurrAddr, "", *STI, OS);
           OS << "\n";
           SegmentCurrAddr += InstSize;
         }
 
       } else {
+        while (SegmentCurrAddr < SegmentEndAddr) {
+          OS.indent(2);
+          OS << llvm::formatv("// {0:x}: ", SegmentCurrAddr - LoadBase);
+          printBytes(
+              llvm::ArrayRef(reinterpret_cast<uint8_t *>(SegmentCurrAddr), 8),
+              OS);
+          OS << "\n";
+          SegmentCurrAddr += 8;
+        }
       }
+      CodeObjectIdx++;
     }
-    CodeObjectIdx++;
   }
   return llvm::PreservedAnalyses::all();
 };
